b96219a047977d932adc791c702377296c067d0e
YumingHe <yhe@marinsoftware.com>
2015-02-16 20:49:19 -0800

    added routing

--- public/modules/notes/directives/NoteShare.js
+++ public/modules/notes/directives/NoteShare.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('notes')
-    .directive('noteShare', function (MAIN_PANEL_STATE) {
+    .directive('noteShare', ['Authentication', '$location', function (MAIN_PANEL_STATE) {
         return {
             templateUrl: 'modules/notes/views/notes.renderer.view.html',
             scope: {},
             link: function (scope) {
                 scope.viewModel = {};
                 scope.viewModel.mainPanelState = MAIN_PANEL_STATE.COURSE_NOTES;
-                console.log(scope.viewModel);
 
                 scope.toggleView = function() {
                     if (scope.viewModel.mainPanelState === MAIN_PANEL_STATE.COURSE_NOTES) {
@@ -17,11 +16,17 @@ angular.module('notes')
                     else {
                         scope.viewModel.mainPanelState = MAIN_PANEL_STATE.COURSE_NOTES;
                     }
-                    console.log(scope.viewModel.mainPanelState);
                 };
+            },
+            controller: function($scope, Authentication, $location) {
+                // This provides Authentication context.
+                $scope.authentication = Authentication;
+
+                // If user is not signed in then redirect to sign in page
+                if (!$scope.authentication.user) $location.path('/signin');
             }
         };
-    })
+    }])
     .constant('MAIN_PANEL_STATE', {
         COURSE_OUTLINE: 'courseOutline',
         COURSE_NOTES: 'courseNotes'
