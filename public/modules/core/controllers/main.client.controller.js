2c1eef74d0bd6ebb969a52c929934474534c7935
Yuming He <yuming.p.he@gmail.com>
2015-06-16 20:45:11 +0800

    UI changes, accoridon, header

---
+++ public/modules/core/controllers/main.client.controller.js
@@ -0,0 +1,39 @@
+/**
+ * Master Controller
+ */
+
+// NOTE: This is a plugin controller for the togglable sidebar, DO NOT modify
+angular.module('core').controller('MainController', ['$scope', '$cookieStore', MasterCtrl]);
+
+function MasterCtrl($scope, $cookieStore) {
+    /**
+     * Sidebar Toggle & Cookie Control
+     */
+    var mobileView = 992;
+
+    $scope.getWidth = function() {
+        return window.innerWidth;
+    };
+
+    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+        if (newValue >= mobileView) {
+            if (angular.isDefined($cookieStore.get('toggle'))) {
+                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
+            } else {
+                $scope.toggle = true;
+            }
+        } else {
+            $scope.toggle = false;
+        }
+
+    });
+
+    $scope.toggleSidebar = function() {
+        $scope.toggle = !$scope.toggle;
+        $cookieStore.put('toggle', $scope.toggle);
+    };
+
+    window.onresize = function() {
+        $scope.$apply();
+    };
+}
